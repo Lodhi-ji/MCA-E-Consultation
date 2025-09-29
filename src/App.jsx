c9952adff36e348922ce2b37843ca76b74f2d401
Ritik Lodhi <ritiklodhi35@gmail.com>
2025-09-29 18:46:58 +0530

    WIP: saving changes before switching

--- src/App.jsx
+++ src/App.jsx
@@ -4,37 +4,32 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "@/contexts/AuthContext";
 import Index from "./pages/Index";
 import EConsultationLanding from "./pages/EConsultationLanding";
 import ConsultationListing from "./pages/ConsultationListing";
 import FilteredConsultation from "./pages/FilteredConsultation";
 import DocumentDetails from "./pages/DocumentDetails";
-import Auth from "./pages/Auth";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <AuthProvider>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<EConsultationLanding />} />
-            <Route path="/consultation-listing" element={<ConsultationListing />} />
-            <Route path="/filtered-consultation" element={<FilteredConsultation />} />
-            <Route path="/document-details" element={<DocumentDetails />} />
-            <Route path="/auth" element={<Auth />} />
-            <Route path="/demo" element={<Index />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </AuthProvider>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<EConsultationLanding />} />
+          <Route path="/consultation-listing" element={<ConsultationListing />} />
+          <Route path="/filtered-consultation" element={<FilteredConsultation />} />
+          <Route path="/document-details" element={<DocumentDetails />} />
+          <Route path="/demo" element={<Index />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
   </QueryClientProvider>
 );
 
